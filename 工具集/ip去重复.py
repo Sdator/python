3dd37d4acd06bd3540d78ed65aba9a4ded80c63b
Sdator <250740270@qq.com>
2020-03-27 18:48:44 +0800

    无头学习

--- 工具集/ip去重复.py
+++ 工具集/ip去重复.py
@@ -1,9 +1,18 @@
+'''
+@Author: your name
+@Date: 2020-03-19 17:38:42
+@LastEditTime: 2020-03-27 17:17:48
+@LastEditors: Please set LastEditors
+@Description: In User Settings Edit
+@FilePath: \python\工具集\ip去重复.py
+'''
 # -*- coding: utf-8 -*-
 import sys
 import os
-
+import re
 # 修改当前工作目录为脚本运行目录
 os.chdir(os.path.dirname(__file__))
+
 
 def fun(s):
     # 从右边找到符号.的位置 进行切片
@@ -12,12 +21,6 @@ def fun(s):
 
 
 def main(files):
-    # print(sys.argv[0])
-    # print(os.getcwd())
-    # print(__file__)
-    # print(os.path.realpath(__file__))
-    # print(os.path.split(os.path.realpath(__file__))[0])
-    # print(os.path.dirname(os.getcwd()))
     with open(files,  encoding='utf-8') as txt:
         # 读入文件 取所有行 去重复
         listIP = set(txt.readlines())
@@ -27,7 +30,28 @@ def main(files):
             print(v)
 
 
+def GetStr(lstr: str, s: str) -> str:
+    '''
+    截获字符串后段\n
+    @lstr:    用于过滤的开头字符串\n
+    @s:       要处理的字符串\n
+    '''
+    return re.match(r'^{}(.*)'.format(lstr), s).group(1)
+
+
 if __name__ == "__main__":
+    s = regex.match("abab123").group(1)
+    print(s)
 
-    main("ip.txt")
+    # name = "SMSMB14567.av"
+    # # 封装的函数
+    # s = GetStr("SM", name)
+    # print(s)
 
+
+    # print(set([1, 2, 3, 4, 5, 6, 7, 1, 1, 1, 1, 1, 1, 1]))
+    # PY文件 = os.path.basename(sys.argv[0])
+    # 参数 = sys.argv[1]
+    # print(PY文件, 参数)
+
+    # s = re.search("^ab(.*)", "ababbcdefa")
