cc2d84db68c88635205f4fd8027f7577576cf7b2
Sdator <250740270@qq.com>
2020-04-03 04:22:13 +0800

    学习browsermobproxy代理

--- 爬虫类/无头学习.py
+++ 爬虫类/无头学习.py
@@ -47,10 +47,16 @@ import time
 import pickle
 # 获取域名用
 from urllib.parse import urlparse
-# from selenium import webdriver
+from selenium import webdriver
 # selenium扩展版
-from seleniumwire import webdriver
+# from seleniumwire import webdriver
+from browsermobproxy import Server
 
+# 创建代理服务器
+server = Server(
+    r'D:\Git\tools\browsermob-proxy-2.1.4\bin\browsermob-proxy.bat')
+server.start()
+proxy = server.create_proxy()
 
 # 修改当前工作目录为脚本运行目录
 os.chdir(os.path.dirname(__file__))
@@ -58,22 +64,24 @@ os.chdir(os.path.dirname(__file__))
 # 浏览器相关配置
 option = webdriver.ChromeOptions()
 
-# 站点信息
-domian = {"Cookies": {}}
 
 # 谷歌浏览器配置
 def setChromeOption(option):
     # 不加载图片
     prefs = {"profile.managed_default_content_settings.images": 2}
     # 添加实验性质的设置参数
-    option.add_experimental_option("prefs", prefs)
+    # option.add_experimental_option("prefs", prefs)
     # 添加启动参数
-    option.add_argument('--headless')   # 无头模式
+    # option.add_argument('--headless')   # 无头模式
     # option.add_argument('--disable-gpu')
     # option.add_argument('lang=en.UTF-8')   # 设置语言
     # option.add_argument('--proxy-server=http://127.0.0.1:10000') # 设置代理
+    option.add_argument("--proxy-server={0}".format(proxy.proxy))
+    option.add_argument('--ignore-certificate-errors')
 
 # 读取cookies
+
+
 def loadCookies(CookiesName):
     '''
     读入cookies
@@ -87,6 +95,8 @@ def loadCookies(CookiesName):
     return False
 
 # 登录并记录cookies
+
+
 def loginSaveCookies(url):
     '''
     登录网站并保存cookies
@@ -118,8 +128,9 @@ def loginSaveCookies(url):
             return cookies
 
 # 响应请求数据回调
-def custom(req, req_body, res, res_body):
 
+
+def custom(req, req_body, res, res_body):
     path = urlparse(req.path).path
     if path == "/appi4OKpaUS3431/open/column.resourcelist.get/2.0":
         req_body = bytes(str(req_body, "utf-8").replace("page_size%5D=10",
@@ -129,47 +140,60 @@ def custom(req, req_body, res, res_body):
 
 
 def 爬取(url, cookies):
-    # 修改谷歌配置
-    setChromeOption(option)
 
     # 永不超时，获取所有请求
-    opt = {"connection_timeout": None, "ignore_http_methods": [],
-           'custom_response_handler': custom}
+    # opt = {"connection_timeout": None, "ignore_http_methods": [],
+    #        'custom_response_handler': custom}
     # 捕获请求和响应
     # opt = {'custom_response_handler': custom}
 
+    # 修改谷歌配置
+    setChromeOption(option)
+
     # 创建浏览器
-    driver = webdriver.Chrome(chrome_options=option, seleniumwire_options=opt)
-    # driver = webdriver.Chrome()
+    # driver = webdriver.Chrome(chrome_options=option, seleniumwire_options=opt)
+    driver = webdriver.Chrome(chrome_options=option)
 
     # 捕获特定请求
-    driver.scopes = [
-        '.*/appi4OKpaUS3431/open/.*'
-    ]
+    # driver.scopes = [
+    #     '.*/appi4OKpaUS3431/open/.*'
+    # ]
 
+    proxy.headers({"cookies": "{}={}".format(
+        cookies[0]['name'], cookies[0]['value'])})
+
+    # 开启代理监控
+    # 开启代理监控，如果不监控会拿不到请求内容
+    # 捕获 头 、内容主体、二进制数据
+    proxy.new_har("login", options={
+                  'captureContent': True, 'captureHeaders': True, 'captureBinaryContent': True})
+    # proxy.har
     # 访问页面
     driver.get(url)
     # 使用 cookies
-    for cookieObj in Cookies:
-        # obj = {k: v for k, v in cookieObj.items() if re.match(r'^value$|^name$', k)}
-        # 获取 cookies 对象中的包含 var、name的键值
-        driver.add_cookie(
-            {"value": cookieObj['value'], "name": cookieObj['name']})
+    # for cookie in cookies:
+    #     # obj = {k: v for k, v in cookieObj.items() if re.match(r'^value$|^name$', k)}
+    #     # 获取 cookies 对象中的包含 var、name的键值
+    #     driver.add_cookie(
+    #         {"value": cookie['value'], "name": cookie['name']})
 
+    driver.implicitly_wait(10)
     # 使用 cookies 后需要重新访问页面
     driver.get(url)
 
     # 延迟处理 等待10秒
     driver.implicitly_wait(10)
 
+    driver.back()
+
     # 如果没跳转成功 则表示 cookies 失效
-    if not urlparse(driver.current_url).path == urlparse(url).path:
-        # 关闭浏览
-        driver.quit()
-        # 重新获取 cookies 并重启本程序
-        domian["Cookies"] = loginSaveCookies(url)
-        爬取(url, domian["Cookies"])
-        return
+    # if not urlparse(driver.current_url).path == urlparse(url).path:
+    #     # 关闭浏览
+    #     driver.quit()
+    #     # 重新获取 cookies 并重启本程序
+    #     Cookies = loginSaveCookies(url)
+    #     爬取(url, Cookies)
+    #     return
 
     # ajax = []
     # 等待响应返回
@@ -206,52 +230,51 @@ def 爬取(url, cookies):
     # driver.execute_script(拦截ajax)
     # 找到所有课程
 
-    ids = []
-    # 获取课程的数量
-    els = len(driver.find_elements_by_css_selector("div.contentMain"))
-    for i in range(els):
-        # 获取课程节点
-        node = driver.find_elements_by_css_selector("div.contentMain")[i]
-        if not node:
-            # driver.quit()
-            return False
-        # 删除旧的请求
-        del driver.requests
-        node.click()
+    # ids = []
+    # # 获取课程的数量
+    # els = len(driver.find_elements_by_css_selector("div.contentMain"))
+    # for i in range(els):
+    #     # 获取课程节点
+    #     node = driver.find_elements_by_css_selector("div.contentMain")[i]
+    #     if not node:
+    #         # driver.quit()
+    #         return False
+    #     # 删除旧的请求
+    #     del driver.requests
+    #     node.click()
 
-        # driver.execute_script()
+    # driver.execute_script()
 
-        # 捕获所有 post 发送请求 和 已经响应了的请求
-        # isfai = True
-        # while isfai:
-        #     driver.implicitly_wait(10)
-        #     for request in driver.requests:
-        #         path = urlparse(request.path).path
-        #         if path == "/appi4OKpaUS3431/open/column.resourcelist.get/2.0":
-        #             # request.body = bytes(
-        #             #     str(request.body, "utf-8").replace("page_size%5D=10", "page_size%5D=100"), encoding="utf8")
-        #             print('\n============', path, '=================\n')
-        #             isfai = False
-        #             break
+    # 捕获所有 post 发送请求 和 已经响应了的请求
+    # isfai = True
+    # while isfai:
+    #     driver.implicitly_wait(10)
+    #     for request in driver.requests:
+    #         path = urlparse(request.path).path
+    #         if path == "/appi4OKpaUS3431/open/column.resourcelist.get/2.0":
+    #             # request.body = bytes(
+    #             #     str(request.body, "utf-8").replace("page_size%5D=10", "page_size%5D=100"), encoding="utf8")
+    #             print('\n============', path, '=================\n')
+    #             isfai = False
+    #             break
 
-        # 获取请求头判断是否ajax请求 判断字典是否存在key
-        # if ('x-requested-with' in request.headers) or (request.method == "POST"):
-        # if request.method == "POST":
-        #     print('\n====', request.path, '====\n')
-        #     # ajax.append(request)
-        #     print(request)
-        # if urlparse(request.path).path == '/appi4OKpaUS3431/open/column.resourcelist.get/2.0':
+    # 获取请求头判断是否ajax请求 判断字典是否存在key
+    # if ('x-requested-with' in request.headers) or (request.method == "POST"):
+    # if request.method == "POST":
+    #     print('\n====', request.path, '====\n')
+    #     # ajax.append(request)
+    #     print(request)
+    # if urlparse(request.path).path == '/appi4OKpaUS3431/open/column.resourcelist.get/2.0':
 
-        # request = driver.wait_for_request(
-        # '/appi4OKpaUS3431/open/column.resourcelist.get/2.0')
-        driver.wait_for_request(
-            '/appi4OKpaUS3431/open/column.resourcelist.get/2.0')
-        # # 返回的二进制响应数据转为str再转为json
-        # request = json.loads(str(request.response.body, "utf-8"))
-        # # # 保存捕获的id
-        # for data in request['data']:
-        #     ids.append((data['title'], data['id']))
-        driver.back()
+    # request = driver.wait_for_request(
+    # '/appi4OKpaUS3431/open/column.resourcelist.get/2.0')
+    # driver.wait_for_request(
+    #     '/appi4OKpaUS3431/open/column.resourcelist.get/2.0')
+    # # 返回的二进制响应数据转为str再转为json
+    # request = json.loads(str(request.response.body, "utf-8"))
+    # # # 保存捕获的id
+    # for data in request['data']:
+    #     ids.append((data['title'], data['id']))
 
     # print(ids)
 
@@ -279,7 +302,7 @@ if __name__ == "__main__":
     # 读入 cookies 不存在则 重新登录获取
     Cookies = loadCookies(hostname) or loginSaveCookies(url)
 
-    domian["Cookies"] = Cookies
+    # domian["Cookies"] = Cookies
 
     # 如果 cookies 不存在
     if not Cookies:
