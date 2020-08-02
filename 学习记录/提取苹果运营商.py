490c277feef6c7fa5ddeee80f2a44b3cb3e8f986
Sdator <250740270@qq.com>
2020-08-03 05:36:37 +0800

    解析完成

--- 学习记录/提取苹果运营商.py
+++ 学习记录/提取苹果运营商.py
@@ -3,7 +3,7 @@
 import asyncio
 import os
 import requests
-import xml.dom.minidom as dom  # xml处理模块
+from lxml import etree  # xml处理模块
 
 
 async def get运营商文件(url):
@@ -13,13 +13,42 @@ async def get运营商文件(url):
 
 
 async def main():
-    url = r"http://ax.phobos.apple.com.edgesuite.net/WebObjects/MZStore.woa/wa/com.apple.jingle.appserver.client.MZITunesClientCheck/version"
-    response = await asyncio.create_task(get运营商文件(url))
-    name = url.rsplit("/")[-1]
-    fileName = os.path.realpath(name)
-    with open(fileName, mode='w') as file_object:
-        file_object.write(response.text)
-        # print(111)
-        print(response, 44444)
+    # 下载苹果相关文件
+    # url = r"http://ax.phobos.apple.com.edgesuite.net/WebObjects/MZStore.woa/wa/com.apple.jingle.appserver.client.MZITunesClientCheck/version"
+    # response = await asyncio.create_task(get运营商文件(url))
+    # txt = response.text
+    # name = url.rsplit("/")[-1]
+    # fileName = os.path.realpath(name)
+    # with open(fileName, mode='w') as file_object:
+    #     file_object.write(txt)
+
+    # # 从字符串解释
+    # node = dom.parseString(txt)
+    filePath = os.path.realpath("version.xml")
+    root = etree.parse(filePath).getroot()
+    keys = root.xpath("/plist/dict/dict/key")
+
+    arr = []
+    for key in keys:
+        if key.text == "ChinaTelecom_USIM_cn":
+            # 获取下一个同胞节点
+            dicts = key.getnext()
+            IOS版本 = dicts.xpath("key/text()")[:-1]
+            运营商版本 = dicts.xpath("dict/string[1]/text()")
+            下载地址 = dicts.xpath("dict/string[2]/text()")
+            i = 0
+            for v in IOS版本:
+                arr.append(
+                    {
+                        "IOS版本": IOS版本[i],
+                        "运营商版本": 运营商版本[i],
+                        "下载地址": 下载地址[i]
+                    })
+                i += 1
+    print(arr)
+
+    # for v in dicts:
+    #     print(v.text)
+
 
 asyncio.run(main())
