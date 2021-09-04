9a5cc884ff51118e5dda68e9b7c27a5b55fc6c94
Sdator <250740270@qq.com>
2021-09-05 07:51:59 +0800

    修复选择目录触发条件还有变量改个名

--- wow/大脚插件自动更新.py
+++ wow/大脚插件自动更新.py
@@ -8,6 +8,8 @@ by 绝 2019.10.6  QQ 250740270
     conda install pyinstaller   # 打包exe
     conda install requests      # 网络通信
 
+    conda install --name wow flake8 -y # 代码检测
+
 生成二进制文件
 
     -F 表示生成单个可执行文件
@@ -35,12 +37,12 @@ import tkinter as tk            # 组件
 from tkinter import filedialog  # 选择框
 import tkinter.messagebox       # 消息框
 import ctypes  # 弹窗
-
 import time
 
-configFile = "config.json"
-# 配置文件 = os.path.normcase(configFile)
 
+# 引用类型可以直接被函数读取并修改
+配置信息 = dict()
+配置文件 = "config.json"
 
 预设配置信息 = {
     "游戏路径": "",
@@ -48,8 +50,6 @@ configFile = "config.json"
     "线程": 10,
     "历史": ['http://wow.bfupdate.178.com/BigFoot/Interface/classic/Interface.1.13.2.18.zip', ]
 }
-# 引用类型可以直接被函数读取并修改
-配置信息 = dict()
 
 # 获取当前脚本路径
 # path1 = sys.path[0]
@@ -60,8 +60,9 @@ def 组合地址(版本号):
 
 
 def 选择游戏目录():
-    # 检测路径是否存在
-    if not os.path.exists(配置信息["游戏路径"]):
+    # 判断key是否存在的正规写法
+    # 如果配置中不存在路径则触发路径选择
+    if not('游戏路径' in 配置信息):
         '''打开选择文件夹对话框'''
         # 初始化tk
         root = tk.Tk()
@@ -69,9 +70,11 @@ def 选择游戏目录():
         root.withdraw()
         # 打开文件对话框
         选择的文件夹 = filedialog.askdirectory(
-            title='选择魔兽世界根目录如：X:\Games\World of Warcraft\\') or os.getcwd()  # 获得选择好的文件夹
-        # Filepath = filedialog.askopenfilename() #获得选择好的文件
-        配置信息["游戏路径"] = os.path.normcase(选择的文件夹+"\_classic_")
+            title=r'选择魔兽世界根目录如：X: \Games\World of Warcraft') or os.getcwd()
+        # 获得选择的文件
+        # Filepath = filedialog.askopenfilename()
+        # 获得选择的文件夹
+        配置信息["游戏路径"] = os.path.normcase(选择的文件夹 + "\\_classic_")
 
 
 def 读入配置(path):
@@ -99,19 +102,19 @@ def 读入配置(path):
             print("json解析错误 使用预设配置")
             return 预设配置信息
         except json.decoder.JSONDecodeError as e:
-            print("json解析失败 使用预设配置")
+            print("json解析失败 使用预设配置,错误信息：", e)
             return 预设配置信息
 
 
 def 写出配置(data):
     # 覆盖原有
-    with open(configFile, "w", encoding='utf-8') as json_file:
+    with open(配置文件, "w", encoding='utf-8') as json_file:
         # 把dict对象转为json并允许非ASCII字符
         json_file.write(json.dumps(data, ensure_ascii=False))
 
 
 def msg(标题, 内容):
-    WS_EX_TOPMOST = 0x40000
+    # WS_EX_TOPMOST = 0x40000
     MB_SYSTEMMODAL = 0x1000
     ctypes.windll.user32.MessageBoxW(
         0, 内容, 标题, MB_SYSTEMMODAL)
@@ -120,7 +123,7 @@ def msg(标题, 内容):
 def 获取插件(配置):
 
     odltime = time.time()
-    print('开始时间：%s' % odltime)
+    print('开始时间:{odltime}')
 
     # 历史版本
     urls = []
@@ -129,7 +132,7 @@ def 获取插件(配置):
 
     for i in range(配置["线程"]):
         # 取最后一个递增版本号
-        url = re.sub(r'\d+(?=.zip$)', str(int(版本尾)+i), 组合地址(配置信息["当前版本"]))
+        url = re.sub(r'\d+(?=.zip$)', str(int(版本尾) + i), 组合地址(配置信息["当前版本"]))
 
         # print('版本尾：%d' % (int(版本尾)+i))
         # print('url:%s' % url)
@@ -138,7 +141,7 @@ def 获取插件(配置):
         r = requests.get(url, stream=True)
         响应代码 = r.status_code
 
-        print(响应代码, (int(版本尾)+i), 777777)
+        print(响应代码, (int(版本尾) + i), 777777)
 
         # 如果有响应就添加到数组
         if 响应代码 == 200:
@@ -146,11 +149,13 @@ def 获取插件(配置):
             urls.append(url)
         # 当前一个响应可用而最后一个不可用
         if 上一个响应 == 200 and 响应代码 == 404:
-            return
+            break
         上一个响应 = 响应代码
 
-    print('url:%s' % url)
-    print('完成时间：%s' % time.time())
+    print(f'url: {url}')
+    print("所花时间 {:.2f} 秒".format(time.time() - odltime))
+
+    return
     sys.exit(0)
 
     if not len(urls):
@@ -192,12 +197,15 @@ def 获取插件(配置):
 
 
 def 打开文件夹():
-    os.system("start "+配置信息["游戏路径"])
+    if os.path.exists(配置信息["游戏路径"]):
+        os.system("start " + 配置信息["游戏路径"])
+    else:
+        msg("找不到目录", "压缩包存在问题或解压失败")
 
 
 def main():
     # 更新全局变量的值 由于无法直接赋值但可以使用对象方法
-    配置信息.update(读入配置(configFile))
+    配置信息.update(读入配置(配置文件))
     选择游戏目录()
     获取插件(配置信息)
     写出配置(配置信息)
